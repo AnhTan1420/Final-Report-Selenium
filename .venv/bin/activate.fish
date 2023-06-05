3dde4485d3596aad0ba0831e534bac6808bff86b
AnhTan1420 <levananhtan.it19a2b@gmail.com>
2023-06-05 23:34:58 +0700

    fix pytest run command

--- .venv/bin/activate.fish
+++ .venv/bin/activate.fish
@@ -30,7 +30,7 @@ end
 # Unset irrelevant variables.
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV "/mnt/Local/Report-Final/.venv"
+set -gx VIRTUAL_ENV "/home/anhtan/Final-Report-Selenium/.venv"
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/bin" $PATH
